df6f3b915fc2ddbfd1242b4f17c4b29de4091972
Your Name <you@example.com>
2026-03-27 18:30:18 +0100

    Move project to root and enable environment variables

--- frontend/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://knnfgyedoxtywwlhazqg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtubmZneWVkb3h0eXd3bGhhenFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0MTc0NDgsImV4cCI6MjA4OTk5MzQ0OH0.9vYZ070VLlYWIDcImJZukJWxoxMjoav1RhBW5fEfY90'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
